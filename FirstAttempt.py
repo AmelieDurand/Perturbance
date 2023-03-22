fa143981d80100efa105eb2d20add20b19845c60
thi-mh <58277062+thi-mh@users.noreply.github.com>
2023-03-22 11:46:22 -0400

    Script with 3 options + read file

--- FirstAttempt.py
+++ FirstAttempt.py
@@ -1,29 +1,41 @@
-from random import randint, choice, choices
-#initialize LIST of useless characters <--- GLOBAL
-# for each: letter, number, special char
-num = [0,1]
-char = list('bjouxz')
+import random
 
-sample = 'MFLLHEYDIFWAFLIIASLIPILAFWISALLAPVREGPEKLSSYESGIEPMGGAWLQFRIRYYMFALVFVVFDVETVFLYPWAMSFDVLGISVFIEAFIFVLILVVGLVYAWRKGALEWS'
+#initialize LIST of useless characters + char <--- GLOBAL
+char_useless = list('BJOUXZ'.lower()) #<--- REMOVE lower() later
+char_main = list('ACDEFGHIKLMNPQRSTVWY'.lower())
+
+#Pass 1 sequence string here
+#sequence = 'MFLLHEYDIFWAFLIIASLIPILAFWISALLAPVREGPEKLSSYESGIEPMGGAWLQFRIRYYMFALVFVVFDVETVFLYPWAMSFDVLGISVFIEAFIFVLILVVGLVYAWRKGALEWS'
 
 #General insert randomly
-def insert1(noise):
-  """ Randomly select a spot to insert ONE thing by concatenating in 3 parts"""
-  try:
-    assert type(noise) is str
-    
-    i = randint(1, len(sample))
-    return sample[:i] + noise + sample[i:]
-  except:
-    print('noise input should be STRING')
+def insert(sample,p=0.01, char_type=char_useless,spread=True):
+  """ Randomly select a spot to insert ONE thing by concatenating in 3 parts
+      take in noise as list"""
+  noise_ls = gen_noise(sample,p,char_type,spread)
+  sample = list(sample)
+  while bool(noise_ls): #<--- noise_ls is random
+    sample.insert(random.randint(1,len(sample)),noise_ls.pop()) #<-- MUST shuffle ls later
+  return ''.join(sample)
+
+def gen_noise(sample,p,char_type,spread): #<-- frequency probability 
+  length = len(sample)
+  noise_ls = random.choices(char_type, k=int(p*length))
+  random.shuffle(noise_ls)
+  return noise_ls
 
 #DECIDE what to insert to one of specific case (TOTAL 8 cases)
-# list of WHAT to insert
-# Calculate HOW MANY to insert (frequency)
+# list of WHAT to insert: homogenous vs mixed
+# Calculate HOW MANY to insert (frequency function of length): 0.1%, 1%, 10%
+
 # decide HOW to insert (tgether or spread)
-def spread_insert(noise_ls):
-  noise_ls = choices(char, k=5)
+
   
 
-
-print(insert1(choice(char)))
+with open(".\\data\\train_data.fa",'r') as f:
+    database = f.readlines()
+p=0.01
+f_out = open(f".\\perturb\\test_data_perturb_{str(p)}.fa",'w') #<------- Need to specify type of perturb
+for i in range(0,len(database),2):
+  f_out.write(database[i])
+  f_out.write(insert(database[i+1]))  #<--- Fix: what if pass extra val to insert
+f_out.close()
