a919458622af6a6576109477355fa8a244d6b4d8
thi-mh <58277062+thi-mh@users.noreply.github.com>
2023-03-22 18:40:09 -0400

    Improve file naming

--- perturb.py
+++ perturb.py
@@ -1,42 +1,48 @@
 import random
 
-#initialize LIST of useless characters + main
+#initialize LIST of useless characters + char <--- GLOBAL
 char_useless = list('BJOUXZ'.lower()) #<--- REMOVE lower() later
 char_main = list('ACDEFGHIKLMNPQRSTVWY'.lower())
 
+#Pass 1 sequence string here
 #sequence = 'MFLLHEYDIFWAFLIIASLIPILAFWISALLAPVREGPEKLSSYESGIEPMGGAWLQFRIRYYMFALVFVVFDVETVFLYPWAMSFDVLGISVFIEAFIFVLILVVGLVYAWRKGALEWS'
 
-def insert(sample,p=0.01, char_type=char_useless,spread=True):
-  """ Randomly select a spot to insert noise by breaking up sequence to list"""
+#General insert randomly
+def insert(sample,p, char_type,spread):
+  """ Randomly select a spot to insert ONE thing by concatenating in 3 parts
+      take in noise as list"""
   noise_ls = gen_noise(sample,p,char_type,spread)
   sample = list(sample)
-  while bool(noise_ls): #<--- noise_ls is random, perform until noise_ls empty
-    sample.insert(random.randint(1,len(sample)),noise_ls.pop()) 
+  while bool(noise_ls): #<--- noise_ls is random
+    sample.insert(random.randint(1,len(sample)),noise_ls.pop()) #<-- MUST shuffle ls later
   return ''.join(sample)
 
 def gen_noise(sample,p,char_type,spread): #<-- frequency probability 
-  """
-  DECIDE what to insert to one of specific case (TOTAL 12 variations)
-  --list of WHAT to insert: homogenous vs mixed
-  --calculate HOW MANY to insert (frequency function of length): 0.1%, 1%, 10%
-  --decide HOW to insert (together or spread)
-  """
   length = len(sample)
   noise_ls = random.choices(char_type, k=int(p*length))
-  if not spread:
-    return [''.join(noise_ls)] #<--- Noise all bunched up
   random.shuffle(noise_ls)
   return noise_ls
 
+#DECIDE what to insert to one of specific case (TOTAL 8 cases)
+# list of WHAT to insert: homogenous vs mixed
+# Calculate HOW MANY to insert (frequency function of length): 0.1%, 1%, 10%
 
+# decide HOW to insert (tgether or spread)
 
   
 
 with open(".\\data\\train_data.fa",'r') as f:
     database = f.readlines()
+
+# CHANGE THE OPTIONS HERE
 p=0.01
-f_out = open(f".\\perturb\\test_data_perturb_{str(p)}.fa",'w') #<------- Need to specify type of perturb
+char_type= char_main
+spread=True
+
+f_out = open(f".\\perturb\\test_data_perturb_{str(p)+char_type[0]+str(spread)}.fa",'w') #<------- Need to specify type of perturb
+
+
 for i in range(0,len(database),2):
   f_out.write(database[i])
-  f_out.write(insert(database[i+1]))  
+  f_out.write(insert(database[i+1],p,char_type,spread))  #<--- Fix: what if pass extra val to insert
 f_out.close()
