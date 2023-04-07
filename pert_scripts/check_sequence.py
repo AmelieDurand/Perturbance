74c0179451e163f84a83877cbb19238e388ca286
thi-mh <58277062+thi-mh@users.noreply.github.com>
2023-04-07 11:30:20 -0400

    Update check_sequence.py
    todo: test script

--- pert_scripts/check_sequence.py
+++ pert_scripts/check_sequence.py
@@ -1,47 +1,62 @@
 
-from Bio import SeqIO
 import pickle
-import gzip
-import random
 
 with open("perturb\\saved_dictionary.pkl", 'rb') as f:
     label_dict = pickle.load(f)
-print(random.sample(label_dict.keys(), k=1))
-print('DKGFGFITPADGSKDVFVHFSAIQSNDFKTLDEGQKVEFSIENGAK' in label_dict.keys())
-print('DKGFGFITPADGSKDVFVHFSAIQSNDFKTLDEG' in label_dict.keys())
 
+def is_label_same(seq_pert, label_pert):
+    """
+    Pass if given sequence, an id is not found
+    Pass if given sequence, the id remains the same
+    Alert if given seequence, the id changes
+    """
+    label_pert = label_pert[1:] #get rid of ">" at the start
+    if seq_pert in label_dict.keys():
+        if label_dict[seq_pert] != label_pert: #<-- check if \n at the end disrupts
+            return False
+    return True
 
-"""
-Structure:
-Seq('MYAIIETGGKQIKVEAGQEIYVEKLAGEVGDVVTFDKVLFVGGDSAKVGVPFVD...INA')
-sp|XXXX|...
-"""
-
-"""
-
-with gzip.open("uniprot_sprot.fasta.gz",'rt') as seq_bank:
-    for record in SeqIO.parse(seq_bank, "fasta"):
-        print(record.id)
+#---- RUNNING
+file_perturb = "perturb\\test_data_perturb_0.01aTrue.fa"
+with open(file_perturb) as f_pert:
+    data = f_pert.readlines()
+    odd_seq = []
+    for i in range(0, len(data), 2):
+        if not is_label_same(data[i+1],data[i]):
+            odd_seq.append(data[i])
         
 
-fasta_sequences = SeqIO.parse(open(".\\data\\test_data.fa"),'fasta')
-#with open('test.txt') as out_file:
-for fasta in fasta_sequences:
-    name, sequence = fasta.id, str(fasta.seq)
-    print(name)
-    #new_sequence = some_function(sequence)
-    #write_fasta(out_file)
+
+
+
+""" --------------- Note to Self
+Uniprot syntax:
+Seq('MYAIIETGGKQIKVEAGQEIYVEKLAGEVGDVVTFDKVLFVGGDSAKVGVPFVD...INA')
+sp|XXXX|...
+
+deepgoplus Sequence:
+label starts with ">"
+
+Already Checked:
+import random
+print(random.sample(label_dict.keys(), k=20))
+print('DKGFGFITPADGSKDVFVHFSAIQSNDFKTLDEGQKVEFSIENGAK' in label_dict.keys()) #<-- True (obtained from sample)
+print('DKGFGFITPADGSKDVFVHFSAIQSNDFKTLDEG' in label_dict.keys()) #<-- False
+
 """
-# Access Bio package (installed)
-# Access python file to check
 
 
-"""------ FINISHED RUNNING
+
+""" INSTRUCTION
+1-- Download uniprot_sprot.fasta.gz and put in same folder as script
+2-- FIRST RUN
+from Bio import SeqIO
+import gzip
+
 label_dic = {}
 with gzip.open("perturb\\uniprot_sprot.fasta.gz",'rt') as seq_bank:
     for record in SeqIO.parse(seq_bank, "fasta"):
         label_dic[record.seq] = record.id
-
 with open('perturb\\saved_dictionary.pkl', 'wb') as f:
     pickle.dump(label_dic, f)
 """
