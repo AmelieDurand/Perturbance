86692a578ee410533e13f3ba8324276a60c9a244
root <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2023-04-11 19:11:38 +0000

    setup vscode dev container

--- run_scripts/run_perturb.py
+++ run_scripts/run_perturb.py
@@ -3,11 +3,12 @@ import csv
 import subprocess
 import click as ck
 
-seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+seeds = [1, 2]
 # pert_chances = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 pert_chances = [0.1]
 cwd = os.getcwd()
-pert_script_path = os.path.join(cwd, "pert_scripts", "perturb.py")
+pert_script_path = os.path.join(cwd, "pert_scripts", "insert_swap.py")
 deepgoplus_script_path = os.path.join(cwd, "deepgoplus", "main-no-diamond.py")
 pkl_script_path = os.path.abspath("convert_tsv_pkl.py")
 eval_script_path = os.path.abspath("evaluate_deepgoplus.py")
@@ -45,7 +46,6 @@ def format_result(results: str, filename: str, iteration: int, pert: float):
     help="Type of perturbation to apply",
 )
 def main(type):
-    pert_chance = 0.1
     for pert_chance in pert_chances:
         for index, seed in enumerate(seeds):
             # Adds perturbations
