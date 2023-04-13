1072423b2c75879bccf13d904943289593bcf7bd
DanielValentini <122065140+DanielValentini@users.noreply.github.com>
2023-04-12 20:57:07 -0400

    Update deletion.py

--- pert_scripts/deletion.py
+++ pert_scripts/deletion.py
@@ -1,5 +1,6 @@
 import random
 
+
 def delete(sample, p):
     sample = list(sample)
     length = len(sample)
@@ -7,16 +8,17 @@ def delete(sample, p):
     indices_to_delete = random.sample(range(length), amount_deletions)
     for i in sorted(indices_to_delete, reverse=True):
         del sample[i]
-    return ''.join(sample)
+    return "".join(sample)
 
-#--------------------------------
+
+# --------------------------------
 # CHANGE THE OPTIONS HERE
 p = 0.01
 
-with open(".\\data\\test_data.fa",'r') as f:
+with open(".\\data\\test_data.fa", "r") as f:
     database = [line.strip() for line in f.readlines()]
-f_out = open(f".\\test_data_delete_{str(p)}.fa",'w') 
-for i in range(0,len(database),2):
-    f_out.write(database[i] + '\n')
-    f_out.write(delete(database[i+1], p) + '\n') 
+f_out = open(f".\\test_data_delete_{str(p)}.fa", "w")
+for i in range(0, len(database), 2):
+    f_out.write(database[i] + "\n")
+    f_out.write(delete(database[i + 1], p) + "\n")
 f_out.close()
