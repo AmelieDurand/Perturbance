2af4eb2a1d5ce6b625637ef22e998877618b6686
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2023-04-27 13:18:15 -0400

    updated evaluate script to be more stable by sorting

--- evaluate_deepgoplus.py
+++ evaluate_deepgoplus.py
@@ -16,11 +16,14 @@ import math
 from utils import FUNC_DICT, Ontology, NAMESPACES
 from matplotlib import pyplot as plt
 import json
+import os
+import pickle
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 
 @ck.command()
+@ck.option("--data-root", "-dr", default="deepgoplus/data", help="Data root folder")
 @ck.option(
     "--train-data-file",
     "-trdf",
@@ -47,7 +50,26 @@ logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 )
 @ck.option("--ont", "-o", default="mf", help="GO subontology (bp, mf, cc)")
 @ck.option("--alpha", "-a", default=50, help="Alpha for for combining scores")
-def main(train_data_file, test_data_file, terms_file, diamond_scores_file, ont, alpha):
+@ck.option("--score-file", "-sf", default="/workdir/eval_score_file")
+def main(
+    data_root,
+    train_data_file,
+    test_data_file,
+    terms_file,
+    diamond_scores_file,
+    ont,
+    alpha,
+    score_file,
+):
+    if os.path.exists(data_root):
+        train_data_file = os.path.join(data_root, train_data_file)
+        # test_data_file = os.path.join(data_root, test_data_file)
+        terms_file = os.path.join(data_root, terms_file)
+        diamond_scores_file = os.path.join(data_root, diamond_scores_file)
+        last_release_metadata = os.path.join(data_root, "metadata/last_release.json")
+        go_rels = Ontology(os.path.join(data_root, "data/go.obo"), with_rels=True)
+
+    # last_release_metadata = 'data-1.0.6/metadata/last_release.json'
     last_release_metadata = "/deepgoplus/metadata/last_release.json"
     with open(last_release_metadata, "r") as f:
         last_release_data = json.load(f)
@@ -64,9 +86,15 @@ def main(train_data_file, test_data_file, terms_file, diamond_scores_file, ont, 
 
     annotations = train_df["prop_annotations"].values
     annotations = list(map(lambda x: set(x), annotations))
+    # annotations = [sorted(list(i)) for i in annotations]
+    # print(annotations)
     test_annotations = test_df["prop_annotations"].values
     test_annotations = list(map(lambda x: set(x), test_annotations))
-    go_rels.calculate_ic(annotations + test_annotations)
+    # test_annotations = [sorted(list(i)) for i in test_annotations]
+    go_rels.calculate_ic(
+        [sorted(list(i)) for i in annotations]
+        + [sorted(list(i)) for i in test_annotations]
+    )
 
     # Print IC values of terms
     ics = {}
@@ -99,7 +127,7 @@ def main(train_data_file, test_data_file, terms_file, diamond_scores_file, ont, 
             for p_id, score in sim_prots.items():
                 allgos |= annotations[prot_index[p_id]]
                 total_score += score
-            allgos = list(sorted(allgos))
+            allgos = list(sorted(allgos))  # modification
             sim = np.zeros(len(allgos), dtype=np.float32)
             for j, go_id in enumerate(allgos):
                 s = 0.0
@@ -117,6 +145,7 @@ def main(train_data_file, test_data_file, terms_file, diamond_scores_file, ont, 
     go_set.remove(FUNC_DICT[ont])
     labels = test_df["prop_annotations"].values
     labels = list(map(lambda x: set(filter(lambda y: y in go_set, x)), labels))
+    labels = [sorted(list(i)) for i in labels]  # modification
     # print(len(go_set))
     deep_preds = []
     # alphas = {NAMESPACES['mf']: 0.55, NAMESPACES['bp']: 0.59, NAMESPACES['cc']: 0.46}
@@ -131,9 +160,11 @@ def main(train_data_file, test_data_file, terms_file, diamond_scores_file, ont, 
         annots_dict = blast_preds[i].copy()
         for go_id in annots_dict:
             annots_dict[go_id] *= alphas[go_rels.get_namespace(go_id)]
+
         for j, score in enumerate(row.preds):
             go_id = terms[j]
             score *= 1 - alphas[go_rels.get_namespace(go_id)]
+
             if go_id in annots_dict:
                 annots_dict[go_id] += score
             else:
@@ -181,11 +212,12 @@ def main(train_data_file, test_data_file, terms_file, diamond_scores_file, ont, 
             new_annots = set()
             for go_id in annots:
                 new_annots |= go_rels.get_anchestors(go_id)
+            new_annots = sorted(list(new_annots))  # modification
             preds.append(new_annots)
 
         # Filter classes
         preds = list(map(lambda x: set(filter(lambda y: y in go_set, x)), preds))
-
+        preds = [sorted(list(i)) for i in preds]  # modification
         fscore, prec, rec, s, ru, mi, fps, fns = evaluate_annotations(
             go_rels, labels, preds
         )
@@ -196,40 +228,41 @@ def main(train_data_file, test_data_file, terms_file, diamond_scores_file, ont, 
         # print(f'{avg_fp} {avg_ic}')
         precisions.append(prec)
         recalls.append(rec)
-        # print(f'Fscore: {fscore}, Precision: {prec}, Recall: {rec} S: {s}, RU: {ru}, MI: {mi} threshold: {threshold}')
+        # print(f'Fscore: {fscore}, Precision: {prec}, Recall: {rec}, S: {s}, RU: {ru}, MI: {mi}, threshold: {threshold}')
         if fmax < fscore:
             fmax = fscore
             tmax = threshold
         if smin > s:
             smin = s
+
     print(f"threshold: {tmax}")
-    print(f"Smin: {smin:0.3f}")
-    print(f"Fmax: {fmax:0.3f}")
+    print(f"Smin: {smin}")
+    print(f"Fmax: {fmax}")
     precisions = np.array(precisions)
     recalls = np.array(recalls)
     sorted_index = np.argsort(recalls)
     recalls = recalls[sorted_index]
     precisions = precisions[sorted_index]
     aupr = np.trapz(precisions, recalls)
-    print(f"AUPR: {aupr:0.3f}")
-    plt.figure()
-    lw = 2
-    plt.plot(
-        recalls,
-        precisions,
-        color="darkorange",
-        lw=lw,
-        label=f"AUPR curve (area = {aupr:0.2f})",
-    )
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel("Recall")
-    plt.ylabel("Precision")
-    plt.title("Area Under the Precision-Recall curve")
-    plt.legend(loc="lower right")
-    plt.savefig(f"results/aupr_{ont}_{alpha:0.2f}.pdf")
-    df = pd.DataFrame({"precisions": precisions, "recalls": recalls})
-    df.to_pickle(f"results/PR_{ont}_{alpha:0.2f}.pkl")
+    print(f"AUPR: {aupr}")
+    # plt.figure()
+    # lw = 2
+    # plt.plot(
+    #     recalls,
+    #     precisions,
+    #     color="darkorange",
+    #     lw=lw,
+    #     label=f"AUPR curve (area = {aupr:0.2f})",
+    # )
+    # plt.xlim([0.0, 1.0])
+    # plt.ylim([0.0, 1.05])
+    # plt.xlabel("Recall")
+    # plt.ylabel("Precision")
+    # plt.title("Area Under the Precision-Recall curve")
+    # plt.legend(loc="lower right")
+    # plt.savefig(f'results/aupr_{ont}_{alpha:0.2f}.pdf')
+    # df = pd.DataFrame({'precisions': precisions, 'recalls': recalls})
+    # df.to_pickle(f'results/PR_{ont}_{alpha:0.2f}.pkl')
 
 
 def compute_roc(labels, preds):
@@ -246,23 +279,24 @@ def compute_mcc(labels, preds):
 
 
 def evaluate_annotations(go, real_annots, pred_annots):
-    total = 0
+    total = np.float64(0.0)
     p = 0.0
     r = 0.0
-    p_total = 0
-    ru = 0.0
-    mi = 0.0
+    p_total = 0.0
+    ru = np.float64(0.0)
+    mi = np.float64(0.0)
     fps = []
     fns = []
     for i in range(len(real_annots)):
         if len(real_annots[i]) == 0:
             continue
         tp = set(real_annots[i]).intersection(set(pred_annots[i]))
-        fp = pred_annots[i] - tp
-        fn = real_annots[i] - tp
-        for go_id in fp:
+        # print(type(pred_annots[i]), type(tp))
+        fp = set(pred_annots[i]) - tp
+        fn = set(real_annots[i]) - tp
+        for go_id in sorted(fp):  # modification
             mi += go.get_ic(go_id)
-        for go_id in fn:
+        for go_id in sorted(fn):  # modification
             ru += go.get_ic(go_id)
         fps.append(fp)
         fns.append(fn)
@@ -276,6 +310,8 @@ def evaluate_annotations(go, real_annots, pred_annots):
             p_total += 1
             precision = tpn / (1.0 * (tpn + fpn))
             p += precision
+
+    # print(type(ru), type(mi))
     ru /= total
     mi /= total
     r /= total
@@ -284,6 +320,8 @@ def evaluate_annotations(go, real_annots, pred_annots):
     f = 0.0
     if p + r > 0:
         f = 2 * p * r / (p + r)
+    # s = mpmath.sqrt(ru * ru + mi * mi)
+    # s = np.sqrt(np.float64(ru) * np.float64(ru) + np.float64(mi) * np.float64(mi), dtype=np.float64)
     s = math.sqrt(ru * ru + mi * mi)
     return f, p, r, s, ru, mi, fps, fns
 
